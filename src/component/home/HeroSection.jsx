513408bf800e49369b1b15e58f82fe4711815168
chibuike <chrisroyalty001@gmail.com>
2024-10-08 17:46:31 +0100

    update update

--- src/component/home/HeroSection.jsx
+++ src/component/home/HeroSection.jsx
@@ -26,9 +26,11 @@ function HeroSection() {
       <div className="w-[80%] grid lg:grid-cols-2 grid-cols-1 items-center gap-16">
         <div className="intro">
           <h1 className="lg:text-4xl text-2xl font-bold text-gray-800 mb-4 leading-10 md:leading-[200px]">
-            FIND, CONNECT, WRITE.
+            Find, Connect, Write
             <br />
-            The ResearchBreed Way (Your Writing Ally)
+            The ResearchBreed Way
+            <br />
+            (Your Writing Ally)
           </h1>
           <p className="text-lg text-black-100 mb-6 font-bold">
             Your source for new research opportunities
